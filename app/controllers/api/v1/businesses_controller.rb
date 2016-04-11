3737d9c90721cf3119d241b3f2ccb29816c6d83d
Donald Lee <donaldlee50@gmail.com>
2016-04-11 02:33:29 -0700

    Added some error handling to businesses#index

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -6,17 +6,21 @@ class Api::V1::BusinessesController < ApplicationController
 	
 	def index
 		# Example of using parameters: http://api.ownlocalbusiness.dev/businesses?per_page=60&page=10 
-		@businesses = Business.order(id: :asc).page(params[:page]).per(params[:per_page])
-		render json: @businesses, meta: { pagination:
+		
+		begin
+			@businesses = Business.order(id: :asc).page(params[:page]).per(params[:per_page])
+			render json: @businesses, meta: { pagination:
                                    { per_page: params[:per_page],
                                      total_pages: @businesses.total_pages,
                                      total_objects: @businesses.total_count } }
+		rescue Exception => e
+			render json: "There was an error processing your request. Please try again", status: 400
+		end
+		
 	end
 
 	def show
 		respond_with :api, :v1, Business.find(params[:id])
 	end
-
-
 end
 
